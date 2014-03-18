1cd9b197026af4c1f3f6b75a486e7e638dedbc68
Dominik Steiner <dominik.j.steiner@gmail.com>
2014-03-18 03:51:40 -0600

    remove forcing for syncing with database

--- examples/websocket.js
+++ examples/websocket.js
@@ -30,7 +30,7 @@ var Registration = sequelize.define("Registration", {
   });
 
 sequelize
-  .sync({ force: true })
+  .sync()
   .complete(function(err) {
     if (!!err) {
       console.log('An error occurred while create the table:', err)
